a8b04dfa18b5c1a3c7e3dc1ece23f09dfb4a52a1
JKTrick75 <david.borde75@gmail.com>
2025-12-17 00:48:42 +0100

    chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,7 +3,11 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
+export default async function Page(props: { 
+  params: Promise<{ 
+    id: string 
+  }>;
+}) {
   const params = await props.params;
   const id = params.id;
   const [invoice, customers] = await Promise.all([
